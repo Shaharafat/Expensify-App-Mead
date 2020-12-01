ba2d4789132994b728108c67013c0ad68f316613
shaharafhat <shaharafhat@gmail.com>
2020-12-01 21:16:13 +0600

    basic setup and routing completed

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,29 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/app.js',
+  mode: "development",
+  entry: "./src/app.js",
   output: {
-    path: path.join(__dirname, '/public'),
-    filename: 'bundle.js'
+    path: path.join(__dirname, "/public"),
+    filename: "bundle.js",
+    publicPath: "/",
   },
   module: {
     rules: [
       {
-        loader: 'babel-loader',
+        loader: "babel-loader",
         test: /\.js$/,
-        exclude: /node_modules/
-      }, {
+        exclude: /node_modules/,
+      },
+      {
         test: /\.s?css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader',
-        ]
-        
-      }
-    ]
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
   },
-  devtool: 'eval-cheap-module-source-map',
+  devtool: "eval-cheap-module-source-map",
   devServer: {
-    contentBase:path.join(__dirname, 'public'),
-    historyApiFallback: true
-  }
-
+    contentBase: path.join(__dirname, "public"),
+    historyApiFallback: true,
+  },
 };
