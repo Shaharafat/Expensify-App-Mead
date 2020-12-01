5df4e74d3b0f07c28c7258471cdc6b7ad0326409
shaharafhat <shaharafhat@gmail.com>
2020-12-02 01:37:07 +0600

    basic redux actions setup

--- src/playground/redux101.js
+++ src/playground/redux101.js
@@ -1,22 +1,34 @@
 import { createStore } from "redux";
 // createStore is a function.
 
+const incrementCount = (payload = {}) => ({
+  type: "INCREMENT",
+  incrementBy: typeof payload.incrementBy === "number" ? payload.incrementBy : 1,
+});
+
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case "INCREMENT": {
       return {
-        count: state.count + 1,
+        count: state.count + action.incrementBy,
       };
     }
     case "DECREMENT": {
+      const decrementBy =
+        typeof action.decrementBy === "number" ? action.decrementBy : 1;
       return {
-        count: state.count - 1,
+        count: state.count - decrementBy,
       };
     }
     case "RESET": {
       return {
-        count : 0
-      }
+        count: 0,
+      };
+    }
+    case "SET": {
+      return {
+        count: action.count,
+      };
     }
     default: {
       return state;
@@ -24,24 +36,33 @@ const store = createStore((state = { count: 0 }, action) => {
   }
 });
 
-console.log(store.getState());
-
-store.dispatch({
-  type: "INCREMENT",
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
 });
-console.log(store.getState());
 
-store.dispatch({
-  type: "DECREMENT",
-});
-console.log(store.getState());
+store.dispatch(incrementCount());
+
+// state will change but it will be ignored.
+// unsubscribe();
 
 store.dispatch({
   type: "DECREMENT",
 });
-console.log(store.getState());
+
+store.dispatch(incrementCount());
+
+store.dispatch({
+  type: "DECREMENT",
+  decrementBy: 10,
+});
+
+store.dispatch(incrementCount({ incrementBy: 3 }));
 
 store.dispatch({
   type: "RESET",
 });
-console.log(store.getState());
+
+store.dispatch({
+  type: "SET",
+  count: 101,
+});
