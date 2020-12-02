978cba7f7a1290910715b0de63a10f2e5ddc26a2
shaharafhat <shaharafhat@gmail.com>
2020-12-02 19:02:14 +0600

    organized redux to connect to expensify app

--- src/app.js
+++ src/app.js
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import AppRouter from './routers/AppRouter';
+import AppRouter from "./routers/AppRouter";
+import configureStore from "./store/configureStore";
 
+import "../node_modules/normalize.css/normalize.css";
+import "./styles/styles.scss";
 
-import '../node_modules/normalize.css/normalize.css'
-import './styles/styles.scss'
+import { addExpense } from "./actions/expenses";
+import { setTextFilter } from "./actions/filters";
+import  getVisibleExpenses  from "./selectors/expenses";
+
+const store = configureStore();
+
+store.dispatch(addExpense({ description: "Water bill" }));
+store.dispatch(addExpense({ description: "Gas bill" }));
+store.dispatch(setTextFilter("Water"));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+console.log(visibleExpenses);
+
 
 ReactDOM.render(<AppRouter />, document.getElementById("app"));
