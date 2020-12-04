114f2402691bd608465db14384b37c7ce976db84
shaharafhat <shaharafhat@gmail.com>
2020-12-05 02:14:26 +0600

    test suite for three more components done

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,24 +1,26 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import ExpenseListItem from './ExpenseListItem'
-import selectExpenses from '../selectors/expenses'
-const ExpenseList = (props) => (
+import React from "react";
+import { connect } from "react-redux";
+import ExpenseListItem from "./ExpenseListItem";
+import selectExpenses from "../selectors/expenses";
+export const ExpenseList = (props) => (
   <div>
     Expense List
-    {
-      props.expenses.map(expense => (
+    {props.expenses.length === 0 ? (
+      <p>No expenses</p>
+    ) : (
+      props.expenses.map((expense) => (
         <ExpenseListItem key={expense.id} {...expense} />
       ))
-    }
+    )}
   </div>
-)
+);
 
 const mapStateToProps = (state) => {
   return {
     // expenses: state.expenses,
     // filters: state.filters
-    expenses: selectExpenses(state.expenses,state.filters),
-  }
-}
+    expenses: selectExpenses(state.expenses, state.filters),
+  };
+};
 
-export default connect(mapStateToProps)(ExpenseList)
+export default connect(mapStateToProps)(ExpenseList);
