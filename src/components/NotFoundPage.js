bc002ee042feac09cc0e4438c96fe74733cca502
shaharafhat <shaharafhat@gmail.com>
2020-12-10 11:34:44 +0600

    testing for auth.js reducer. done

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-
+require("history").createBrowserHistory;
 export const NotFoundPage = () => (
   <div>
     404! - <Link to="/">Go Home</Link>
--- src/tests/reducers/auth.test.js
+++ src/tests/reducers/auth.test.js
@@ -1,2 +1,18 @@
 import authReducer from '../../reducers/auth'
 
+test('should setup login correctly', () => {
+  const action = {
+    type: 'LOG_IN',
+    uid: '123'
+  }
+  const state = authReducer({}, action);
+  expect(state.uid).toBe(action.uid)
+})
+
+test('should setup logout correctly', () => {
+  const action = {
+    type: 'LOG_OUT'
+  }
+  const state = authReducer({uid: 'anything'}, action);
+  expect(state).toEqual({})
+})
